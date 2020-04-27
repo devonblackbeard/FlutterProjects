7125c7afc39bf839ec4adb229c6e28d4d73afb93
Devon Blackbeard <32315720+devonblackbeard@users.noreply.github.com>
2020-04-26 22:46:30 -0600

    Added in listview

--- autodetail_app/lib/main.dart
+++ autodetail_app/lib/main.dart
@@ -1,4 +1,11 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:http/http.dart' as http;
+import 'package:cached_network_image/cached_network_image.dart';
+//import 'package:carousel_slider/carousel_slider.dart';
+//import 'carouselView.dart';
 
 void main() => runApp(MyApp());
 
@@ -36,59 +43,114 @@ class MyHomePage extends StatefulWidget {
   // case the title) provided by the parent (in this case the App widget) and
   // used by the build method of the State. Fields in a Widget subclass are
   // always marked "final".
-
   final String title;
-
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  List imageData;
+  String urlpath = "http://api.opendota.com/api/heroStats";
+
+  Future<String> getData() async {
+    var response = await http.get(
+        Uri.encodeFull(urlpath),
+        headers: {"Accept": "application/json"});
+
+    setState(() {
+      imageData = json.decode(response.body);
+    });
+    return "success";
+  }
+
   @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+  Widget build(BuildContext context)
+  {
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text(widget.title)
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.all(12.0),
-              child: Text(
-                'LA Auto Detailing',
-                style: Theme.of(context).textTheme.display1,
-              ),
-            ),
-          ],
-        ),
-      ),
-      // This trailing comma makes auto-formatting nicer for build methods.
+      body: buildListView(),
     );
   }
+
+  Widget buildListView()
+  {
+    return ListView.builder(
+      padding: const EdgeInsets.all(0),
+      itemCount: imageData == null ? 0 : imageData.length,
+      itemBuilder: (context,index)
+      {
+        return buildImageColumn(imageData[index]);
+      });
+  }
+
+  Widget buildImageColumn(dynamic item) => Container(
+    decoration: BoxDecoration(color: Colors.purple),
+    margin: const EdgeInsets.fromLTRB(0, 3, 0, 6),
+    child: Column(
+      children: [
+        new CachedNetworkImage(
+          //imageUrl: item['img']+ "/"+urlpath ,
+          imageUrl: "https://api.opendota.com" + item['img'],
+          placeholder: (context, url) => new CircularProgressIndicator(),
+          errorWidget: (context, url, error) => new Icon(Icons.error),
+          fadeOutDuration: new Duration(seconds: 2),
+          fadeInDuration: new Duration(seconds: 4),
+        ),
+        buildRow(item)
+      ],
+    ),
+  );
+
+Widget buildRow(dynamic item) {
+  return ListTile(
+      title: Align(
+        child: new Text(
+            item['localized_name'] == null ? '' : item['localized_name']),
+        alignment: Alignment(0, 0),
+      ),
+      onTap: () {
+        showDialog(
+            context: context,
+            child: new AlertDialog(
+              titlePadding: new EdgeInsets.symmetric(vertical: 2.0, horizontal: 2),
+              // contentPadding: EdgeInsets.fromLTRB(12,0,32,0),
+              elevation: 50.0,
+              shape: RoundedRectangleBorder(
+                  borderRadius: new BorderRadius.circular(18)
+              ),
+              backgroundColor: Colors.lightGreen,
+              title: new Column(
+                children: <Widget>[
+                  new Text("You clicked:", ),
+                ],
+              ),
+              content: new Text(item['localized_name']),
+              actions: <Widget>[
+                new FlatButton(
+//                  onPressed: () {
+//                    Navigator.push(
+//                      context,
+//                      MaterialPageRoute(
+//                        builder: (context) => DetailScreen(
+//                            name: item['name'],
+//                            attackType: item['attack_type']),
+//                      ),
+//                    );
+//                  },
+//                  child: new Text('OK'),
+                ),
+              ],
+            ));
+      });
+}
+
+
+@override
+void initState() {
+  super.initState();
+  // Call the getData() method when the app initializes
+  this.getData();
+}
 }
